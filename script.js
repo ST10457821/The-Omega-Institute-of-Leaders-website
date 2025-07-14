fc5ac9e85078a12d98dd807d40ae22d7e7f082ad
Les <45718863+garvageart@users.noreply.github.com>
2025-07-14 15:18:36 +0200

    changes from les

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+function toggleDropdown () {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+// Close the dropdown if clicked outside
+window.onclick = function (event) {
+    if (!event.target.matches('dropdown-button')) {
+        var dropdowns = document.getElementByClassName("dropdown-content");
+        for (var i = 0; i < dropdown.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdoww.classList, contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('dropdownButton');
     const menu = document.getElementById('dropdownMenu');
 
     button.addEventListener('click', () => {
-        meny.classList.toggle('show');
+        menu.classList.toggle('show');
     });
 
     //Toggle open/close on button click
     button.addEventListener('click', (e) => {
         e.stopPropagation(); //prevent window listenr form instantly closing it 
         menu.classList.toggle('show');
-    })
+    });
 
     //Close dropdown when clicking outside
     window.addEventListener('click', (e) => {
-        of (!button.contains(e.target)) {
+        if (!button.contains(e.target)) {
             menu.classList.remove('show');
         }
     });
